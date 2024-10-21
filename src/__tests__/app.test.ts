5305f9fb98e69cc6df5c3ce273e9a5b55f8fd6c4
nickoff <nick.olehnovich@gmail.com>
2024-10-21 17:33:53 +0300

    feat: add tests

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -1,23 +1,43 @@
 import request from 'supertest';
 import { server } from '../modules/server';
 
-test('GET /api/users should return all existing users. Scenario 1', async () => {
-  const response = await request(server).get('/api/users');
+describe(' Scenario 1. Test GET method', () => {
+  test('GET /api/users should return all existing users.', async () => {
+    const response = await request(server).get('/api/users');
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(response.body)).toBe(true);
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  test('GET /api/users/{userId} should return user with id === userId if it exists', async () => {
+    const mockUser = {
+      username: 'Mikola',
+      age: 40,
+      hobbies: [],
+    };
+
+    const responsePost = await request(server).post('/api/users').send(mockUser);
+    const { id } = responsePost.body;
+
+    const responseGet = await request(server).get(`/api/users/${id}`);
+
+    expect(responseGet.status).toBe(200);
+    expect(responseGet.body.id).toBe(id);
+  });
 });
 
-test('POST /api/users should create record about new user and store. Scenario 2', async () => {
-  const mockUser = {
-    username: 'Mikola',
-    age: 40,
-    hobbies: [],
-  };
+describe('Scenario 2. Test POST method', () => {
+  test('POST /api/users should create record about new user and store.', async () => {
+    const mockUser = {
+      username: 'Mikola',
+      age: 40,
+      hobbies: [],
+    };
 
-  const response = await request(server).post('/api/users').send(mockUser);
+    const response = await request(server).post('/api/users').send(mockUser);
 
-  expect(response.status).toBe(201);
-  expect(response.body.username).toEqual(mockUser.username);
-  expect(response.body.age).toEqual(mockUser.age);
+    expect(response.status).toBe(201);
+    expect(response.body.username).toEqual(mockUser.username);
+    expect(response.body.age).toEqual(mockUser.age);
+  });
 });
