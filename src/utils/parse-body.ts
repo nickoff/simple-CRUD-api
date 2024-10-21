3a46d7419d5cad225edc15a3f1343cb744e82fe3
nickoff <nick.olehnovich@gmail.com>
2024-10-21 13:41:50 +0300

    fix: fix validation User object

--- src/utils/parse-body.ts
+++ src/utils/parse-body.ts
@@ -2,8 +2,14 @@ import { type IncomingMessage } from 'node:http';
 import { type UserModel } from 'models/user.model';
 
 function isUserModel(user: UserModel): boolean {
-  if ('username' in user && 'age' in user && 'hobbies' in user) {
-    const { username, age, hobbies } = user;
+  const { username, age, hobbies } = user;
+  const allowedProps = ['username', 'age', 'hobbies'];
+  if (
+    'username' in user &&
+    'age' in user &&
+    'hobbies' in user &&
+    !Object.keys(user).some((key) => !allowedProps.includes(key))
+  ) {
     return (
       typeof username === 'string' &&
       typeof age === 'number' &&
